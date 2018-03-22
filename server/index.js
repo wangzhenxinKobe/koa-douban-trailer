ada936f5611233b87bcbe8ed6be069e8dee0f87a
向振宇 <xzylzz@163.com>
2018-03-22 16:02:50 +0800

    koa集成pug模板5-6

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const Koa = require('koa')
+const views = require('koa-views')
+
+const { resolve } = require('path')
+
+const app = new Koa()
+app.use(views(resolve(__dirname, './views'), {
+  extension: 'pug'
+}))
+app.use(async (ctx, next) => {
+  await ctx.render('index', {
+    you: 'xxx',
+    me: 'aaa'
+  })
+})
+
+app.listen(4455)
